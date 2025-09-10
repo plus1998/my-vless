3d368adbd4afb4f1fff31d90756aeb921abf190f
plus1998 <plus.1998@qq.com>
2025-09-11 04:42:17 +0800

    fix

--- vless.sh
+++ vless.sh
@@ -25,9 +25,7 @@ bash <(curl -Ls https://raw.githubusercontent.com/plus1998/my-vless/main/d11.sh)
 
 # 配置数据库
 echo "配置数据库..."
-cd /etc/x-ui/
-rm -rf x-ui.db
-wget https://raw.githubusercontent.com/plus1998/my-vless/main/x-ui.db
+cd /etc/x-ui/ && rm -rf x-ui.db && wget https://raw.githubusercontent.com/plus1998/my-vless/main/x-ui.db
 
 # 重启 x-ui 服务
 echo "重启 x-ui 服务..."
